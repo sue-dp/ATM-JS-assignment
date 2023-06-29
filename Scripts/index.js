20e1de685373a24f34fed39237a582f8a91224b0
Sue Roy <sueroy08@gmail.com>
2023-06-29 14:24:22 -0600

    fixed withdraw function, created Goodbye page with Home button

--- Scripts/index.js
+++ Scripts/index.js
@@ -4,17 +4,18 @@ class ATM {
   }
 
   accountDeposit() {
-    // console.log(this.currentBalance);
     const deposit = parseFloat(document.getElementById("amount-textbox").value);
-    // console.log(deposit);
 
     if (isNaN(deposit) || deposit <= 0) {
+      const responses = document.querySelector(".responses");
+      responses.textContent = `Invalid entry, please try again. Your balance is: $${atm.currentBalance}`;
       return this.currentBalance;
     }
 
     this.currentBalance += deposit;
-    // console.log(this.currentBalance);
-    getBalance();
+    const responses = document.querySelector(".responses");
+    responses.textContent = `You have deposited $${deposit}. Your balance is: $${atm.currentBalance}`;
+    // getBalance();
     return this.currentBalance;
   }
 
@@ -22,29 +23,19 @@ class ATM {
     let withdrawal = parseFloat(
       document.getElementById("amount-textbox").value
     );
-    // if (withdrawal < 0) {
-    //   // console.log("Please enter an amount greater than $0.\n");
-    //   return this.currentBalance;
-    // }
-    if (withdrawal > this.currentBalance) {
+    if (isNaN(withdrawal) || withdrawal <= 0) {
       const responses = document.querySelector(".responses");
-      responses.textContent = "Insufficient Funds";
-      // console.log("You have insufficient funds\n");
-      let newWithdrawal = parseFloat(
-        prompt(
-          "Please enter an amount less than or equal to $" + currentBalance
-        )
-      );
-      withdrawal = newWithdrawal;
-    }
-    if (withdrawal === 0) {
+      responses.textContent = `Invalid entry. Please try again. Your balance is: $${atm.currentBalance}`;
+      return this.currentBalance;
+    } else if (withdrawal > this.currentBalance) {
+      const responses = document.querySelector(".responses");
+      responses.textContent = `Insufficient Funds. Please try again. Your balance is: $${atm.currentBalance}`;
       return this.currentBalance;
     }
-    // console.log(
-    // "You have withdrawn $" + withdrawal + ". Please take your cash."
-    // );
+
     this.currentBalance -= withdrawal;
-    getBalance();
+    const responses = document.querySelector(".responses");
+    responses.innerText = `You have withdrawn $${withdrawal}. Your balance is: $${atm.currentBalance}`;
     return this.currentBalance;
   }
 }
@@ -56,38 +47,17 @@ const getBalance = () => {
 
 const goodbye = () => {
   const responses = document.getElementById("responses");
+  const container = document.getElementById("container");
+  const menu = document.getElementById("menu");
+  const returnButton = document.createElement("button");
   responses.innerText = "Thank You - Goodbye!";
+  container.removeChild(menu);
+  returnButton.className = "return-button";
+  container.appendChild(returnButton);
+  returnButton.textContent = "HOME";
+  returnButton.onclick = () => {
+    location.reload("index.html");
+  };
 };
 
 const atm = new ATM(100);
-// console.log("Welcome to B.O.S.S. ATM!");
-// const account = new ATM(currentBalance);
-
-// while (true) {
-//   const responses = document.querySelector(".responses");
-//   responses.textContent = "You deposited $500";
-//   const newResponse = document.createElement(newResponse);
-//   newResponse.type = "text";
-//   newResponse.id = "myInput";
-//   newResponse.placeholder = "Your current balance is: $" + currentBalance;
-
-//   responses.appendChild(newResponse);
-//   // const menu =
-//   // "Please choose from the following options:\n1: Balance Check\n2: Deposit\n3: Withdrawal\n4: Quit\n>>>";
-//   // const user_input = prompt(menu);
-
-//   if (user_input === "1") {
-//     // console.log("Your current balance is: $" + current_balance);
-//   } else if (user_input === "2") {
-//     currentBalance = ATM.accountDeposit();
-//   } else if (user_input === "3") {
-//     currentBalance = ATM.accountWithdrawal();
-//   } else if (user_input === "4") {
-//     // console.log("Thank you, See you next time!");
-//     break;
-//   } else if (user_input === null) {
-//     // console.log("Thank you, See you next time!");
-//     break;
-//   }
-// }
-// // eventlistner for account currentBalance, depost, withdrawal, exit, back to mm from depost, back to mm from withdrawal
